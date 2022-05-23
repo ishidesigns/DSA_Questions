9ac1441cf96daa237e97cffbf19080dba894669c
ishidesigns <jishika089@gmail.com>
2022-05-24 01:11:28 +0530

    Common Tree Node class

---
+++ Tree/Node.java
@@ -0,0 +1,12 @@
+package DSA_Interview_Questions.Tree;
+
+public class Node {
+	int val;
+    Node left, right;
+    Node(int val) 
+    {
+        this.val = val;
+        left = null;
+        right = null;
+    }
+}
