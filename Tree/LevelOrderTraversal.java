79dd2e900bce04018907d353f90d13bee649d65a
ishidesigns <jishika089@gmail.com>
2022-06-06 23:30:13 +0530

    Level Order Traversal

--- Tree/LevelOrderTraversal.java
+++ Tree/LevelOrderTraversal.java
@@ -2,28 +2,20 @@ package DSA_Interview_Questions.Tree;
 
 import java.util.*;
 
-public class LevelOrderTraversal 
+public class LevelOrderTraversal
 {
-	public static void main(String[] args) 
-	{
-		Node root = new Node(5);
-	    Node rl = new Node(3);
-	    Node rr = new Node(7);
-	    root.left = rl;
-	    root.right = rr;
-	    
-	    Node rll = new Node(2);
-	    Node rlr = new Node(4);
-	    rl.left = rll;
-	    rl.right = rlr;
-	    
-	    Node rrl = new Node(6);
-	    rr.left = rrl;
-	    
-	    printLevelOrder(root);
+	public static void main(String[] args) {
+		Node root = null;
+		root = addNode(root, 5);
+	    addNode(root, 3);
+	    addNode(root, 7);
+	    addNode(root, 2);
+	    addNode(root, 4);
+	    addNode(root, 6);
+	    printTree(root);
 	}
 	
-	public static void printLevelOrder(Node root)
+	static void printTree(Node root)
 	{
 		if(root == null)
 			return;
@@ -52,5 +44,40 @@ public class LevelOrderTraversal
 				System.out.println();
 			}
 		}
+	}
+	
+	static Node addNode(Node root, int n)
+	{
+		Node newNode = new Node(n);
+		if(root == null)
+		{
+			root = newNode;
+		}
+		else
+		{
+			Queue<Node> q = new LinkedList<>();
+			q.add(root);
+			while(!q.isEmpty())
+			{
+				Node front = q.poll();
+				if(front.left == null)
+				{
+					front.left = newNode;
+					return root;
+				}
+				else
+					q.add(root.left);
+				
+				
+				if(front.right == null)
+				{
+					front.right = newNode;
+					return root;
+				}
+				else
+					q.add(root.right);
+			}
+		}
+		return root;
 	}
 }
