0c62333cf571c3b0d1d17a677c66095f82bf0669
ishidesigns <jishika089@gmail.com>
2022-06-24 01:16:10 +0530

    Views of Binary Tree

---
+++ Tree/ViewsOfTree.java
@@ -0,0 +1,178 @@
+package DSA_Interview_Questions.Tree;
+
+import java.util.*;
+
+
+class Pair{
+	int h_dist;
+	Node newNode;
+	Pair(Node newNode, int h_dist)
+	{
+		this.h_dist = h_dist;
+		this.newNode = newNode;
+	}
+}
+public class ViewsOfTree
+{
+	static List<Integer> list, list2;
+	public static void main(String[] args) 
+	{
+		/*
+				  5
+				/   \
+			   3     7
+			  / \   / \
+			 2   4 6   8
+			      
+		*/
+		
+		
+		Node root = new Node(5);
+		Node rl = new Node(3);
+		Node rr = new Node(7);
+		root.left = rl;
+		root.right = rr;
+		
+		Node rll = new Node(2);
+		Node rlr = new Node(4);
+		rl.left = rll;
+		rl.right = rlr;
+		
+		Node rrl = new Node(6);
+		Node rrr = new Node(8);
+		rr.left = rrl;
+		rr.right = rrr;
+		
+		System.out.print("Top View: ");
+		topView(root);
+		System.out.println();
+		
+		System.out.print("Bottom View: ");
+		bottomView(root);
+		System.out.println();
+		
+		System.out.print("Right View: ");
+		rightView1(root);
+		System.out.println();
+		
+		System.out.print("Right View (Optimized): ");
+		list = new ArrayList<>();
+		rightView2(root, 0);
+		for(int i : list)
+			System.out.print(i + " ");
+		System.out.println();
+		
+		System.out.print("Left View (Optimized): ");
+		list2 = new ArrayList<>();
+		leftView(root, 0);
+		for(int i : list2)
+			System.out.print(i + " ");
+		
+	}
+	static void leftView(Node root, int level)
+	{
+		if(root == null)
+			return;
+		
+		if(level == list2.size())
+			list2.add(root.val);
+		
+		leftView(root.left, level + 1);
+		leftView(root.right, level + 1);
+	}
+	
+	// right View with TC : O(n) and SC : O(height of tree)
+	static void rightView1(Node root)
+	{
+        if(root == null) 
+        	return;
+        List<Integer> list = new ArrayList<>();
+        Queue<Node> q = new  LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty())
+        {
+            int size = q.size();
+            for(int i = 0; i < size; i++)
+            {
+                Node node = q.poll();
+                if(i == size - 1)
+                    list.add(node.val);
+
+                if(node.left != null)
+                    q.add(node.left);
+
+                if(node.right != null)
+                    q.add(node.right);
+            }
+        }
+        for(int i : list)
+        	System.out.print(i + " ");
+	}
+	
+	// Optimized Approach
+	static void rightView2(Node root, int level)
+	{
+		if(root == null)
+			return;
+		
+		if(level == list.size())
+			list.add(root.val);
+		
+		rightView2(root.right, level + 1);
+		rightView2(root.left, level + 1);
+	}
+	
+	
+	static void topView(Node root)
+	{
+		if(root == null)
+			return;
+		
+		Map<Integer, Integer> map = new TreeMap<>();
+		Queue<Pair> q = new LinkedList<>();
+		q.add(new Pair(root, 0));
+		while(!q.isEmpty())
+		{
+			Pair p = q.poll();
+			Node curr = p.newNode;
+			int hd = p.h_dist;
+			
+			if(map.get(hd) == null)
+				map.put(hd, curr.val);
+			
+			if(curr.left != null)
+				q.add(new Pair(curr.left, hd - 1));
+			
+			if(curr.right != null)
+				q.add(new Pair(curr.right, hd + 1));
+		}
+		for(int i : map.values())
+			System.out.print(i + " ");
+	}
+	
+	static void bottomView(Node root)
+	{
+		if(root == null)
+			return;
+		
+		Map<Integer, Integer> map = new TreeMap<>();
+		Queue<Pair> q = new LinkedList<>();
+		q.add(new Pair(root, 0));
+		while(!q.isEmpty())
+		{
+			Pair p = q.poll();
+			Node curr = p.newNode;
+			int hd = p.h_dist;
+			
+			map.put(hd, curr.val);
+			
+			if(curr.left != null)
+				q.add(new Pair(curr.left, hd - 1));
+			
+			if(curr.right != null)
+				q.add(new Pair(curr.right, hd + 1));
+		}
+		for(int i : map.values())
+			System.out.print(i + " ");
+	}
+}
