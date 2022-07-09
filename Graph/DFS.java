28bec7656e832262749666ce88c998acaba69840
ishidesigns <jishika089@gmail.com>
2022-07-09 21:15:32 +0530

    Depth First Search

--- Graph/DFS.java
+++ Graph/DFS.java
@@ -1,27 +1,61 @@
-package DSA_Interview_Questions.Graph;
+package Graph;
+
+import java.util.*;
 
 public class DFS 
 {
-	static int n;
-	static int[] arr;
-	static boolean[] visited;
-	public static void main(String[] args) 
-	{
-		n = 10000007;
-		arr = new int[n];
-		visited = new boolean[n];
-		
-	}
-	
-	static void dfs(int curr) {
-		// TC: O(V+E)
-		visited[curr] = true;
-		for(int i : arr) {
-			if(visited[i])
-				continue;
-			
-			dfs(i);
-		}
-	}
-	
+	public static void dfs(int node, boolean vis[], ArrayList<ArrayList<Integer>> adj, ArrayList<Integer> list) {
+        list.add(node);
+        //marking current node as visited
+        vis[node] = true;
+
+        //getting neighbour nodes
+        for(Integer it: adj.get(node)) {
+            if(vis[it] == false) {
+                dfs(it, vis, adj, list);
+            }
+        }
+    }
+    public static ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj)
+    {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        //boolean array to keep track of visited vertices
+        boolean vis[] = new boolean[V+1];
+
+        //If you are starting from node 2, then i should start from 2.
+        for(int i = 1;i<=V;i++) {
+            if(!vis[i]) dfs(i, vis, adj, list);
+        }
+
+        return list;
+    }
+    static void printAns(ArrayList < Integer > ans) {
+        for (int i = 0; i < ans.size(); i++) {
+            System.out.print(ans.get(i) + " ");
+        }
+    }
+    public static void main(String args[]) {
+
+        ArrayList < ArrayList < Integer >> adj = new ArrayList < > ();
+
+        // adding new arraylists to 'adj' to add neighbour nodes
+        for (int i = 0; i < 6; i++) {
+            adj.add(new ArrayList < > ());
+        }
+
+        adj.get(1).add(2);
+        adj.get(1).add(3);
+        adj.get(1).add(4);
+        adj.get(1).add(5);
+        adj.get(2).add(4);
+        adj.get(2).add(1);
+        adj.get(3).add(1);
+        adj.get(4).add(1);
+        adj.get(4).add(2);
+        adj.get(5).add(1);
+
+        ArrayList < Integer > ans = dfsOfGraph(5, adj);
+        printAns(ans);
+    }	
 }
