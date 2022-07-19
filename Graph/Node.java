389e1728c8367917f77e41811400b17ea9de9fb2
ishidesigns <jishika089@gmail.com>
2022-07-19 23:03:11 +0530

    Graph

---
+++ Graph/Node.java
@@ -0,0 +1,35 @@
+package DSA_Interview_Questions.Graph;
+
+import java.util.*;
+
+public class Node implements Comparator <Node>
+{
+    int v;
+    int weight;
+    
+    Node(int _v, int _w) 
+    { 
+    	v = _v; 
+    	weight = _w; 
+    }
+    
+    Node() {}
+    
+    int getV() 
+    { 
+    	return v; 
+    }
+    int getWeight() 
+    { 
+    	return weight; 
+    }
+    
+    @Override
+    public int compare(Node node1, Node node2) {
+        if (node1.weight < node2.weight)
+            return -1;
+        if (node1.weight > node2.weight)
+            return 1;
+        return 0;
+    }
+}
